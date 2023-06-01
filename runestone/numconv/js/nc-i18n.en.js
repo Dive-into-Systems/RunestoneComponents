25ca24cc6f3c5e591a052ece0e348169d37d405a
Yue Zhang <yzhang4@cs.swarthmore.edu>
2023-06-01 17:04:31 -0400

    added number conversion

---
+++ runestone/numconv/js/nc-i18n.en.js
@@ -0,0 +1,10 @@
+$.i18n().load({
+    en: {
+        msg_no_answer: "No answer provided.",
+        msg_NC_check_me: "Check me",
+        msg_NC_compare_me: "Compare me",
+        msg_NC_generate_a_number: "Generate a number",
+        msg_NC_incorrect: "Incorrect. Please think again.",
+        msg_NC_correct: "Correct. Good job!"
+    },
+});
